b7e71c80979973c7edcc013a5481b1b7a4d2758a
elliot9802 <elliot@segerlind.se>
2023-10-20 10:20:22 +0200

    feat(vite-vue): Enhance user experience and optimize SCSS
    - Implement user-friendly error handling for PDF conversion.
    - Refactor and optimize SCSS for better readability and maintainability.
    - Introduce SCSS variables for consistent theming and easier future updates.
    - Add detailed error logging for both URL and HTML conversion failures.
    - Improve form UI elements with hover and disabled states.
    
    Note: Project now utilizes SCSS with Vite/Vue for styled components.

--- WApiVueWebbTest/src/components/PdfConverter.vue
+++ WApiVueWebbTest/src/components/PdfConverter.vue
@@ -1,20 +1,41 @@
 <template>
-    <div>
+    <div class="pdf-converter-container">
+
+        <!-- Overview -->
+        <h2>WebApiPdfConverter Live Demo</h2>
+        <p>Welcome to the WebApiPdfConverter demo. Here you can easily test the API's capabilities by converting a URL or an
+            HTML file to PDF.</p>
+        <a href="#översikt" target="_blank">API Overview</a>
+
+        <!-- Convert URL to PDF Section -->
         <h2>Convert URL to PDF</h2>
-        <form @submit.prevent="convertUrl">
-            <input v-model="url" name="urlInput" placeholder="Enter URL" />
-            <button type="submit" :disabled="isLoading">Convert</button>
+        <form @submit.prevent="convertUrl" class="converter-form">
+            <div class="input-group">
+                <input v-model="url" name="urlInput" placeholder="Enter URL" class="input-field" />
+                <span class="help-icon"
+                    title="Enter the complete web address you want to convert. e.g., http://example.com">?</span>
+            </div>
+            <button type="submit" :disabled="isLoading" class="convert-button">Convert</button>
+            <a href="#konvertera-url-till-pdf" target="_blank">See API Guide for URL to PDF</a>
         </form>
 
+        <!-- Convert HTML File to PDF Section -->
         <h2>Convert HTML File to PDF</h2>
-        <form @submit.prevent="convertHtml">
-            <input type="file" name="htmlFileInput" @change="handleFileUpload" />
-            <button type="submit" :disabled="isLoading">Convert</button>
+        <form @submit.prevent="convertHtml" class="converter-form">
+            <div class="input-group">
+                <input type="file" name="htmlFileInput" @change="handleFileUpload" class="input-field" />
+                <span class="help-icon" title="Upload the HTML file you want to convert.">?</span>
+            </div>
+            <button type="submit" :disabled="isLoading" class="convert-button">Convert</button>
+            <a href="#konvertera-html-fil-till-pdf" target="_blank">See API Guide for HTML to PDF</a>
         </form>
-    </div>
-    <div v-if="isLoading">Loading...</div>
-</template>
 
+        <!-- Loading and Errors -->
+        <div v-if="isLoading" class="loading-indicator">Loading...</div>
+        <div v-if="error" class="error-message">{{ error }}</div>
+
+    </div>
+</template>
 
 <script>
 import axios from 'axios';
@@ -25,13 +46,15 @@ export default {
         return {
             url: '',
             htmlFile: null,
-            isLoading: false
+            isLoading: false,
+            error: null
         };
     },
     methods: {
         async convertUrl() {
+            this.error = null;
             if (!this.url) {
-                console.error("Please provide a valid URL.");
+                this.error = "Please provide a valid URL.";
                 return;
             }
             this.isLoading = true;
@@ -50,6 +73,7 @@ export default {
 
             } catch (error) {
                 console.error("Error converting URL to PDF:", error);
+                this.error = "Failed to convert URL to PDF:";
                 if (error.response && error.response.data instanceof Blob) {
                     const reader = new FileReader();
                     reader.onload = function () {
@@ -63,8 +87,9 @@ export default {
         },
 
         async convertHtml() {
+            this.error = null;
             if (!this.htmlFile) {
-                console.error("Please provide a valid HTML file.");
+                this.error = "Please provide a valid HTML file.";
                 return;
             }
             this.isLoading = true;
@@ -85,6 +110,7 @@ export default {
 
             } catch (error) {
                 console.error('Error converting HTML to PDF:', error);
+                this.error = 'Failed to convert HTML to PDF:';
             } finally {
                 this.isLoading = false;
             }
@@ -104,3 +130,118 @@ export default {
     }
 };
 </script>
+
+<style lang="scss" scoped>
+$primary-color: #3498db;
+$error-color: #e74c3c;
+$light-bg-color: #f6f9fc;
+$text-color: #2f3b52;
+$text-color-light: #2f3b52b4;
+$border-color: #ccc;
+$hover-opacity: 0.8;
+$box-shadow-light: 0 0 15px rgba(0, 0, 0, 0.1);
+$box-shadow-tiny: 0 0 5px rgba(0, 0, 0, 0.1);
+$disabled-bg-color: #bdc3c7;
+
+.pdf-converter-container {
+    width: 100%;
+    max-width: 600px;
+    margin: 50px auto;
+    padding: 30px;
+    box-shadow: $box-shadow-light;
+    border-radius: 10px;
+    background-color: $light-bg-color;
+}
+
+h2 {
+    margin: 50px auto;
+    color: $text-color;
+    font-weight: 500;
+}
+
+p {
+    color: $text-color-light;
+    margin-bottom: 10px;
+}
+
+.api-link {
+    margin: 10px 0;
+}
+
+.converter-form {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    margin-bottom: 30px;
+}
+
+.input-group {
+    position: relative;
+
+    .input-field {
+        padding: 10px 15px;
+        border: 1px solid $border-color;
+        border-radius: 5px;
+        color: grey;
+        font-size: 14px;
+        width: 100%;
+    }
+
+    .help-icon {
+        position: absolute;
+        right: 15px;
+        top: 50%;
+        transform: translateY(-50%);
+        background-color: $primary-color;
+        color: #fff;
+        border-radius: 50%;
+        padding: 5px;
+        width: 20px;
+        height: 20px;
+        text-align: center;
+        cursor: pointer;
+        transition: background-color 0.3s;
+
+        &:hover {
+            opacity: $hover-opacity;
+        }
+    }
+}
+
+.convert-button {
+    padding: 10px 20px;
+    background-color: $primary-color;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: opacity 0.3s ease-in-out;
+
+    &:hover {
+        opacity: $hover-opacity;
+    }
+
+    &:disabled {
+        background-color: $disabled-bg-color;
+        cursor: not-allowed;
+    }
+}
+
+.loading-indicator {
+    position: absolute;
+    bottom: 20px;
+    right: 20px;
+    background-color: #ffeb3b;
+    padding: 5px 10px;
+    border-radius: 3px;
+    box-shadow: $box-shadow-tiny;
+}
+
+.error-message {
+    color: $error-color;
+    background-color: #ffe6e6;
+    padding: 10px;
+    border-radius: 5px;
+    margin-top: 20px;
+}
+</style>
